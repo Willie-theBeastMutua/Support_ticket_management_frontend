d50d3967434a7341f48a47e0f9acdb12883ef868
Wilson Mutua <willymathuva@gmail.com>
2026-01-19 13:40:09 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'support-ticket-app',
+  webDir: 'www'
+};
+
+export default config;
